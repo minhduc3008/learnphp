f82391c30b0ff57c2fa268c178baa40fcd5563e4
minhduc3008 <vuminhduc3008@gmail.com>
2023-06-16 16:28:27 +0700

    register form

---
+++ day2/exam1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="">
+        <label for="">Fullname</label>
+        <input type="text" name="fullname">
+
+        <label for="">Email</label>
+        <input type="text" name="email">
+
+        <label for="">Phone</label>
+        <input type="text" name="phone">
+
+        <label for="">Password</label>
+        <input type="password" name="password">
+
+        <label for="">Address</label>
+        <input type="text" name="address">
+
+        <label for="">Gender</label>
+        <input type="radio" name="gender" value="1">Nam
+        <input type="radio" name="gender" value="2">Nữ
+    </form>
+</body>
+</html>
